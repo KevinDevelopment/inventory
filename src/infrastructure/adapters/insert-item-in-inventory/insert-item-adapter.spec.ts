c465c3b54fad44e2e6c00d247779a21b3f3eb6a9
KevinDevelopment <kevincristhian@outlook.com>
2024-01-05 23:47:43 -0300

    test: add test in insert adapter

---
+++ src/infrastructure/adapters/insert-item-in-inventory/insert-item-adapter.spec.ts
@@ -0,0 +1,33 @@
+import { describe, test, expect, vitest } from "vitest"
+import { MySqlAdapter } from "./insert-item-adapter"
+import { Name, Amount, SerialNumber, TechnicalSpecifications, Owner, Comments, Location } from "../../../core/domain/value-objects"
+import { Inventory } from "../../../core/domain/entities/inventory/inventory"
+
+interface SutTypes {
+  sut: MySqlAdapter
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new MySqlAdapter()
+  return {
+    sut
+  }
+}
+
+describe("MySqlAdapter", () => {
+  test("Should return an item if insert is sucessfully", async () => {
+    const { sut } = makeSut()
+    const inventory = new Inventory(
+      new Name("input.name"),
+      new Amount(400),
+      new SerialNumber("input.name"),
+      new TechnicalSpecifications("input.name"),
+      new Owner("input.name"),
+      new Location("input.name"),
+      new Comments("input.name")
+    )
+    vitest.spyOn(sut, 'add').mockReturnValueOnce(Promise.resolve())
+    const insertItem = await sut.add(inventory)
+    expect(insertItem).toBe(undefined)
+  })
+})
