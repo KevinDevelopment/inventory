ce7e15aaacae3298d564ad6b37f671f2f8c10590
KevinDevelopment <kevincristhian@outlook.com>
2024-01-13 13:30:55 -0300

    refactor: refactored especificações técnicas to error class invalid action

--- src/core/domain/value-objects/technical-specifications.ts
+++ src/core/domain/value-objects/technical-specifications.ts
@@ -1,16 +1,17 @@
 import { MissingParamError } from "../errors/missing-param-error"
 import { InvalidParamError } from "../errors/invalid-param-error"
+import { InvalidAction } from "../errors"
 
 export class TechnicalSpecifications {
   private _value: string
 
   constructor(_value: string) {
     if (!_value) {
-      throw new MissingParamError("technical specifications")
+      throw new InvalidAction("As especificações técnicas do item precisam ser informadas")
     }
 
     if (typeof (_value) !== "string") {
-      throw new InvalidParamError("technical specifications")
+      throw new InvalidAction("As especificações técnicas precisam ser do tipo texto")
     }
 
     this._value = _value
