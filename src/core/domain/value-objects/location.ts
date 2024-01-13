efc4c8e56d677db53a0cbb319878597c932374d2
KevinDevelopment <kevincristhian@outlook.com>
2024-01-13 13:23:39 -0300

    refactor: refactored location to error class invalid action

--- src/core/domain/value-objects/location.ts
+++ src/core/domain/value-objects/location.ts
@@ -1,16 +1,15 @@
-import { MissingParamError } from "../errors/missing-param-error"
-import { InvalidParamError } from "../errors/invalid-param-error"
+import { InvalidAction } from "../errors"
 
 export class Location {
   private _value: string
 
   constructor(_value: string) {
     if (!_value) {
-      throw new MissingParamError("location")
+      throw new InvalidAction("A localização precisa ser informada")
     }
 
     if (typeof (_value) !== "string") {
-      throw new InvalidParamError("location")
+      throw new InvalidAction("A localização precisa ser uma string")
     }
 
     this._value = _value
